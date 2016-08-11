a39bdd40472df83bf2141b49e5208bb475335890
Lucas Theisen <ltheisen@mitre.org>
2016-08-11 11:15:13 -0400

    current state for comment on https://github.com/ivogabe/gulp-typescript/issues/326

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,45 @@ var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', ['copyNodeModules', 'compileTypescript'], function () {});
 
-gulp.task('compileTypescript', function () {
+gulp.task('compileTypescript', ['compileTypescriptGulpSrc'], function () {});
+
+gulp.task('compileTypescriptGulp', function () {
+    // https://github.com/ivogabe/gulp-typescript/issues/326
+    var tsResult = gulp.src(
+        [
+            'src/main/typescript/**/*.ts',
+        ])
+        .pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('target/classes/META-INF/resources/js'));
+});
+
+gulp.task('compileTypescriptGulpSrc', function () {
+    // https://github.com/ivogabe/gulp-typescript/issues/326
+    var tsResult = gulp.src(
+        [
+            '**/*.ts',
+            '!node/**',
+            '!node_modules/**',
+            '!target/**',
+            '!typings/**'
+        ])
+        .pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('target/classes/META-INF/resources/js'));
+});
+
+gulp.task('compileTypescriptTsSrc', function () {
+    // https://github.com/ivogabe/gulp-typescript/issues/326
     var tsResult = tsProject.src() 
         .pipe(ts(tsProject));
 
     return tsResult.js.pipe(gulp.dest('target/classes/META-INF/resources/js'));
 });
 
+
 gulp.task('copyNodeModules', function () {
-    gulp.src('./node_modules/**')
+    gulp.src('node_modules/**')
         .pipe(gulp.dest('target/classes/META-INF/resources/js/node_modules'));
 });
 
